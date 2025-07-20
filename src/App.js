5fb4558bd80bcf4adcab7179e5c7ab91c6fa2069
boshraemad <boshraemad2004@icloud.com>
2025-07-21 00:02:30 +0300

    use json-server to create fake questions Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <DateCounter/>
+      <Header/>
+      <Main>
+        <p>1/15 </p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
