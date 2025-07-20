8a80c002e780cb395e4dd255dfd298b11f737231
boshraemad <boshraemad2004@icloud.com>
2025-07-21 00:25:46 +0300

    fetch Questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import {useEffect , useReducer} from 'react';
 
+const initialState={
+  questions:[],
+  //loading ready error finished
+  state:""
+}
+
+function reducer(state , action){
+  switch(action.type){
+    case "fetchQuestions": return {...state , questions:action.payload , state:"ready"}
+    case "failedToFetch" :return {...state , state:"error"}
+    default:
+      throw new Error("unKnown Action");
+  }
+}
 function App() {
+
+  const [state , dispatch]=useReducer(reducer , initialState);
+
+  useEffect(()=>{
+   const fetchQuestions=async()=>{
+    try{
+      const res=await fetch("http://localhost:8000/questions");
+      const data=await res.json();
+      dispatch({type:"fetchQuestions" , payload:data});
+    }catch(error){
+      dispatch({type:"faildedToFetch"});
+    }
+   }
+
+   fetchQuestions();
+  },[])
+
   return (
     <div className="App">
       <Header/>
