ccece9eddff8266c4939d944a6bc2a11e7bd01d3
boshraemad <boshraemad2004@icloud.com>
2025-07-21 22:57:34 +0300

    handle new answers

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const initialState={
   questions:[],
   //loading ready error active finished
   status:"loading",
-  index:0
+  index:0,
+  answer:null
 }
 
 function reducer(state , action){
@@ -18,13 +19,17 @@ function reducer(state , action){
     case "fetchQuestions": return {...state , questions:action.payload , status:"ready"}
     case "failedToFetch" :return {...state , status:"error"}
     case "start":return {...state , status:"active"}
+    case "newAnswer": return {
+      ...state,
+      answer:action.payload
+    }
     default:
       throw new Error("unKnown Action");
   }
 }
 function App() {
 
-  const [{questions , status , index} , dispatch]=useReducer(reducer , initialState);
+  const [{questions , status , index , answer} , dispatch]=useReducer(reducer , initialState);
   const questionsNumber=questions.length; 
 
   useEffect(()=>{
@@ -48,7 +53,7 @@ function App() {
         { status === "loading" && <Loader/>}
         {status === "error" && <Error/>}
         {status === "ready" && <StartScreen questionsNumber={questionsNumber} dispatch={dispatch}/>}
-        {status === "active" && <Questions question={questions[index]}/>}
+        {status === "active" && <Questions question={questions[index]} dispatch={dispatch} answer={answer}/>}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function Options({question}) {
+export default function Options({question , dispatch , answer}) {
+
+  const inAnswered = answer!==null;
   return (
    <>
-    {question.options.map((option)=>
+    {question.options.map((option , index)=>
     <div className="options">
-    <button className="btn btn-option">{option}</button>
+    <button disabled={inAnswered} className={`btn btn-option ${ index === answer ? "answer" : ""} ${ inAnswered ? question.correctOption === index ? "correct" : "wrong" : ""}`} onClick={()=>{dispatch({type:"newAnswer" , payload:index})}}>{option}</button>
     </div>)}
    </>
   )
